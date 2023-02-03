5a17c1944c67da5b209edcca22922b79718657d6
Guilherme Vieira <Guilherme.torres1609@gmail.com>
2023-02-03 00:13:40 -0300

    adicionando script

---
+++ script.js
@@ -0,0 +1,10 @@
+let show = true;
+
+const listaFocada = document.getElementsByClassName('cards-mesas');
+const listaSalgados = document.getElementsByClassName('lista-salgados');
+const listaSalgados1 = document.getElementsByClassName('container-list');
+const listaSalgados2 = document.getElementsByClassName('div-escondida');
+
+function expandir(){
+    listaFocada.classList.add("on");
+}
