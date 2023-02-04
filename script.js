4a0a095cfc6f150419708c9aa1bfffbf450d1789
Guilherme Vieira <Guilherme.torres1609@gmail.com>
2023-02-04 16:55:41 -0300

    :construction: iniciando script

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let show = true;
 
-const listaFocada = document.getElementsByClassName('cards-mesas');
-const listaSalgados = document.getElementsByClassName('lista-salgados');
-const listaSalgados1 = document.getElementsByClassName('container-list');
-const listaSalgados2 = document.getElementsByClassName('div-escondida');
+const listaFocada = window.document.getElementById("cards-mesas");
+const listaSalgados = window.document.getElementById("lista-salgados");
+const listaSalgados1 = window.document.getElementById("container-list");
+const listaSalgados2 = window.document.getElementById("div-escondida");
+const mostrarMais = window.document.getElementById("Mostrar");
+
+function expandir() {
+    listaFocada.classList.add("on");
+    listaSalgados.classList.add("on");
+    listaSalgados1.classList.add("on");
+    listaSalgados2.classList.add("on");
+    mostrarMais.innerText = "Mostrar menos"
+}
