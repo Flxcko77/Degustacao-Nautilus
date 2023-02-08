f01744fc2912e88dc30817016df67fb37ff9a265
Guilherme Vieira <Guilherme.torres1609@gmail.com>
2023-02-08 13:02:54 -0300

    :truck: adicionando funcionalidades

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 const listaFocada = window.document.getElementById("cards-mesas");
 const listaSalgados = window.document.getElementById("lista-salgados");
 const listaSalgados1 = window.document.getElementById("container-list");
@@ -6,9 +7,11 @@ const listaSalgados2 = window.document.getElementById("div-escondida");
 const mostrarMais = window.document.getElementById("Mostrar");
 
 function expandir() {
-    listaFocada.classList.add("on");
-    listaSalgados.classList.add("on");
-    listaSalgados1.classList.add("on");
-    listaSalgados2.classList.add("on");
-    mostrarMais.innerText = "Mostrar menos"
-}
+         listaFocada.classList.add("on");
+         listaSalgados.classList.add("on");
+         listaSalgados1.classList.add("on");
+         listaSalgados2.classList.add("on");
+         mostrarMais.classList.add("on");
+         mostrarMais.innerText = "Mostrar menos"; 
+         mostrarMais.value = true;
+} 
