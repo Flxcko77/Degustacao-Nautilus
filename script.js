111a57c06964a8cc24da7ed5db9c045284c17b48
Guilherme Vieira <Guilherme.torres1609@gmail.com>
2023-02-11 13:23:50 -0300

    :bulb: adicionando comentarios

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-
-
+/* ---> declarando variaveis <--- */
 const listaFocada = window.document.getElementById("cards-mesas");
 const listaSalgados = window.document.getElementById("lista-salgados");
 const listaSalgados1 = window.document.getElementById("container-list");
 const listaSalgados2 = window.document.getElementById("div-escondida");
 const mostrarMais = window.document.getElementById("Mostrar");
+const AdicionarMesas = window.document.getElementById("maisMesas");
 
+/* ---> expandindo lista da mesa <--*/
 function expandir() {
          listaFocada.classList.add("on");
          listaSalgados.classList.add("on");
@@ -14,4 +15,13 @@ function expandir() {
          mostrarMais.classList.add("on");
          mostrarMais.innerText = "Mostrar menos"; 
          mostrarMais.value = true;
-} 
+
+        
+}
+
+
+
+/* ---> adicionando mais mesas <--- */
+
+
+
